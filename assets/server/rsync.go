7a0924cc8de12be105d3770210f3d044ec164589
aatomu <aatomu21263+git@gmail.com>
2025-11-06 22:02:57 +0900

    ✨ src dirがないときに作成するように

--- assets/server/rsync.go
+++ assets/server/rsync.go
@@ -14,7 +14,14 @@ func getGenerationByTime() ([]string, error) {
 	// Get backup directory
 	entries, err := os.ReadDir(backupDestination)
 	if err != nil {
-		return []string{}, fmt.Errorf("failed to read backup root directory: %w", err)
+		// ディレクトリが存在しない場合は作成を試みる
+		if os.IsNotExist(err) {
+			if createErr := os.MkdirAll(backupDestination, 0755); createErr != nil {
+				return nil, fmt.Errorf("backup destination directory does not exist and cannot be created: %w", createErr)
+			}
+			return []string{}, nil // ディレクトリを作成し、世代は0で返す
+		}
+		return nil, fmt.Errorf("failed to read backup root directory: %w", err)
 	}
 
 	// Listup generations
